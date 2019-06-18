ca8146bcd310ae855ff0f0fea09d1a2fc4101a9e
Arne Coucheron <arco68@gmail.com>
2019-06-18 02:50:20 +0200

    exynos990: Build FlipFlap
    * dependencies: Add FlipFlap.
    
    Change-Id: Iec33f0328b5a3a1f99a7bf9c3013b1963e0ed151
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>

--- common.mk
+++ common.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.samsung
 
+# FlipFlap
+PRODUCT_PACKAGES += \
+    FlipFlap
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
