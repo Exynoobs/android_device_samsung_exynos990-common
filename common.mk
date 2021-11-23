97fe2aa5fc34149f7ed62225ed4c8b28c29fb92e
Tim Zimmermann <tim@linux4.de>
2021-11-23 21:40:36 +0100

    exynos990-common: switch to new AIDL vibrator HAL
    * Sepolicy label moved to slsi_sepolicy repo
    
    Change-Id: If10972cce06dd5128729d4d7409c57f79775b09b

--- common.mk
+++ common.mk
@@ -283,7 +283,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.3-service.samsung
+    android.hardware.vibrator-service.samsung
 
 # WiFi
 PRODUCT_PACKAGES += \
