e228b5e3ffa4477bf7d36df44f41cc6914e6960a
Josip Kelecic <kelecicj@gmail.com>
2022-01-03 19:46:57 +0100

    exynos990-common: update blobs from XXSCEUL7
    * Added shim for libepicoperator
    
    Co-authored-by: Tim Zimmerman <tim@linux4.de>
    Change-Id: I0c0360fd5cf71790edb68b7a0aa4463f22222a06

--- Android.mk
+++ Android.mk
@@ -21,7 +21,7 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.universal990.so
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1
 
 EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
 $(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
