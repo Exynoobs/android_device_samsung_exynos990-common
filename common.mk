13e99a2a4a93eef4c6d095e6c58c9cf10728a1eb
Francescodario Cuzzocrea <bosconovic@gmail.com>
2022-02-11 15:32:45 +0100

    exynos990-common: rename init.exynos990.root.rc as init.samsung.rc
     * this contains samsung modifications to AOSP's system/core init.rc,
       rename it as init.samsung.rc as is traditionally done on samsung
       exynos devices and to make it clear that is samsung modifications
    
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- common.mk
+++ common.mk
@@ -136,7 +136,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/fstab.exynos990:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.exynos990 \
     $(COMMON_PATH)/configs/init/init.exynos990.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos990.rc \
     $(COMMON_PATH)/configs/init/init.exynos990.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos990.usb.rc \
-    $(COMMON_PATH)/configs/init/init.exynos990.root.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.exynos990.root.rc \
+    $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsung.rc \
     $(COMMON_PATH)/configs/init/init.recovery.exynos990.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.exynos990.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
     $(COMMON_PATH)/configs/init/init.udfps.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.udfps.rc
