fd913dd924b02eebde1fc92194c123961e5dd03d
Francescodario Cuzzocrea <bosconovic@gmail.com>
2022-02-26 16:50:32 +0100

    exynos990: build hw/samsung usb hal

--- common.mk
+++ common.mk
@@ -311,7 +311,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.typec
+    android.hardware.usb@1.3-service.samsung
 
 # Vibrator
 PRODUCT_PACKAGES += \
