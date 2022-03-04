0a1be4769f0475d1a4e5ddb9e4b9e57880c21a80
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:27:02 +0000

    exynos990-common: Removed audio A2dp and Hearing Aid from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: Ia1d4fbe0e406e895d295936c524c5dbba6c269cd

--- common.mk
+++ common.mk
@@ -30,7 +30,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hidl.allocator@1.0.vendor \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
