ded8aa22387d2aa4fd877e0d0dcf9f1b2d8a60a5
Tim Zimmermann <tim@linux4.de>
2022-09-16 15:36:09 +0200

    exynos990-common: Use libutils from VNDK32 for HWC and sensors
    * Abort message: 'incStrongRequireStrong() called on 0x7586a3a1c0 which isn't already owned'
    
    Change-Id: Icd5a845ecf5226d5b2c74c5d19ba699eb74acda0

--- common.mk
+++ common.mk
@@ -316,6 +316,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils-v32
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
