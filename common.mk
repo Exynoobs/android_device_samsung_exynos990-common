4ea1e4dcc3621e237377661050efdc410d82cd90
Josip Kelecic <kelecicj@gmail.com>
2022-10-07 00:30:01 +0200

    exynos990-common: drop vendor.lineage.fastcharge

--- common.mk
+++ common.mk
@@ -95,10 +95,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# FastCharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.1-service.samsung
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.samsung
