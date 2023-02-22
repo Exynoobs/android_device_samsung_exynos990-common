fe19092fe2d2b2555c2f4dff7d25b49b8d7b5abe
Francescodario Cuzzocrea <bosconovic@gmail.com>
2023-02-22 08:11:53 +0100

    exynos990-common: switch to exynos thermal hal
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- common.mk
+++ common.mk
@@ -321,10 +321,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.samsung
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal@2.0-service.exynos
 
 # Touch HAL
 PRODUCT_PACKAGES += \
