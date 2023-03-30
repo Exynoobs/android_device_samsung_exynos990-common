9c231a39b55001c3f7a53ba89187b34ce79ac3f2
Francescodario Cuzzocrea <bosconovic@gmail.com>
2023-03-30 23:49:42 +0200

    exynos990-common: match audio hal and impl

--- common.mk
+++ common.mk
@@ -25,8 +25,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio@5.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.0-impl:32 \
     android.hardware.bluetooth.audio-impl \
